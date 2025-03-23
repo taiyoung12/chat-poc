f2f5699b330681f59ba848c39ca433cb4936c4df
taiyoung <qq3222@naver.com>
2025-03-23 23:55:50 +0900

    Feature: chat room creatore service layer

--- src/main/java/us/zep/chatserver/service/chatroom/ChatRoomCreator.java
+++ src/main/java/us/zep/chatserver/service/chatroom/ChatRoomCreator.java
@@ -1,2 +1,19 @@
-package us.zep.chatserver.service.chatroom;public class ChatRoomCreator {
+package us.zep.chatserver.service.chatroom;
+
+import org.springframework.stereotype.Service;
+
+import us.zep.chatserver.entity.ChatRoom;
+import us.zep.chatserver.repository.ChatRoomRepository;
+
+@Service
+public class ChatRoomCreator {
+	private final ChatRoomRepository chatRoomRepository;
+
+	public ChatRoomCreator(ChatRoomRepository chatRoomRepository) {
+		this.chatRoomRepository = chatRoomRepository;
+	}
+
+	public ChatRoom by(String name){
+		return chatRoomRepository.createRoom(name);
+	}
 }
