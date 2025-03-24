1ac3262431f092388a1c096a4796691c94f91089
taiyoung <qq3222@naver.com>
2025-03-24 21:07:50 +0900

    Feature: chat message processor service layer

--- src/main/java/us/zep/chatserver/service/chat/ChatMessageProcessor.java
+++ src/main/java/us/zep/chatserver/service/chat/ChatMessageProcessor.java
@@ -1,2 +1,62 @@
-package us.zep.chatserver.service.chat;public class ChatMessageProcessor {
+package us.zep.chatserver.service.chat;
+
+import org.springframework.stereotype.Service;
+
+import us.zep.chatserver.model.ChatMessage;
+import us.zep.chatserver.redis.RedisPublisher;
+import us.zep.chatserver.repository.history.UserRoomHistoryRepository;
+
+@Service
+public class ChatMessageProcessor {
+	private final RedisPublisher redisPublisher;
+	private final UserRoomHistoryRepository userRoomHistoryRepository;
+
+	public ChatMessageProcessor(RedisPublisher redisPublisher, UserRoomHistoryRepository userRoomHistoryRepository) {
+		this.redisPublisher = redisPublisher;
+		this.userRoomHistoryRepository = userRoomHistoryRepository;
+	}
+
+	public void processMessage(ChatMessage chatMessage) {
+		if (chatMessage.getRoomId() == null) {
+			return;
+		}
+
+		redisPublisher.publish(chatMessage);
+	}
+
+	public boolean processUserEntry(ChatMessage chatMessage) {
+		if (chatMessage.getRoomId() == null || chatMessage.getSender() == null) {
+			return false;
+		}
+
+		if ("JOIN".equals(chatMessage.getType())) {
+			String userId = chatMessage.getSender();
+			String roomId = chatMessage.getRoomId();
+
+			if (!userRoomHistoryRepository.hasEnteredRoom(userId, roomId)) {
+				userRoomHistoryRepository.recordRoomEntry(userId, roomId);
+				redisPublisher.publish(chatMessage);
+				return true;
+			}
+			return false;
+		}
+
+		redisPublisher.publish(chatMessage);
+		return true;
+	}
+
+	public void processUserLeave(ChatMessage chatMessage) {
+		if (chatMessage.getRoomId() == null || chatMessage.getSender() == null) {
+			return;
+		}
+
+		if ("LEAVE".equals(chatMessage.getType())) {
+			String userId = chatMessage.getSender();
+			String roomId = chatMessage.getRoomId();
+
+			userRoomHistoryRepository.removeRoomEntry(userId, roomId);
+		}
+
+		redisPublisher.publish(chatMessage);
+	}
 }
